67ff7f32a9ed4a5115a786f8c89e04e97bc57ca1
Olaitan Majekodunmi <flowsgreen@gmail.com>
2020-03-12 02:20:59 +0100

    7.12 store authenticated user from auth library into database - @test - url: https://console.firebase.google.com/project/crwn-db-9198f/database/firestore/data~2Fusers

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up
 import Header from "./components/header/header.component";
 import "./App.css";
 import { Switch, Route } from "react-router-dom";
-import { auth } from "./firebase/firebase.utils";
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 
 class App extends React.Component {
   constructor(props) {
@@ -18,10 +18,8 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user });
-
-      console.log(user);
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async user => {
+      createUserProfileDocument(user);
     });
   }
 
