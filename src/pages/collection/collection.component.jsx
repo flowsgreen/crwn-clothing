f85dd4acd6519fd862e4d16e09da3db1efe6c582
Olaitan Majekodunmi <flowsgreen@gmail.com>
2020-04-12 04:37:30 +0100

    11.3. Collection Routing and Selector @test - url+ /shop/hats, console log

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import { connect } from "react-redux";
 import collectionItem from "../../components/collection-item/collection-item.component";
+import { selectCollection } from "../../redux/shop/shop.selectors";
 import "./collection.styles.scss";
 
-const collectionPage = ({ match }) => {
-  console.log(match.params.collectionId);
+const collectionPage = ({ collection }) => {
+  console.log(collection);
   return (
     <div className="collection-page">
       <h2>COLLECTION PAGE</h2>
@@ -11,4 +13,8 @@ const collectionPage = ({ match }) => {
   );
 };
 
-export default collectionPage;
+const mapStateToProps = (state, ownProps) => ({
+  collection: selectCollection(ownProps.match.params.collectionId)(state),
+});
+
+export default connect(mapStateToProps)(collectionPage);
